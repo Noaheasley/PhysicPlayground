49a7be53b7e8ad01d48e43d0542efa57ceca56c9
Noah <Noaheasley408@gmail.com>
2021-09-17 16:02:11 -0500

    added a winning script, fixed some bugs with the tank, and changed map

--- Assets/Scripts/ScoreBoardBehaviour.cs
+++ Assets/Scripts/ScoreBoardBehaviour.cs
@@ -8,6 +8,10 @@ public class ScoreBoardBehaviour : MonoBehaviour
     public Text CoinCounter;
     private int _score = 0;
 
+    public int Score()
+    {
+        return _score;
+    }
     // Start is called before the first frame update
     void Update()
     {
---
+++ Assets/Scripts/WinningBehaviour.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinningBehaviour : MonoBehaviour
+{
+    public GameObject nuke;
+    public Canvas scoreBoard;
+
+    private void Update()
+    {
+        if(scoreBoard.GetComponent<ScoreBoardBehaviour>().Score() == 8)
+        {
+            nuke.GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
